79d1a0f554ca82eaf7331d04cdcce37fb75498d3
Hugo <hugo.arenas@usach.cl>
2021-12-20 18:38:34 -0300

    vista usuario v01

---
+++ front-altea/src/views/VistaUsuario.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './VistaUsuario.css';
+
+function VistaUsuario() {
+    return (
+      <div className="VistaUsuario">
+        <header className="VistaUsuario-header">
+          <p>
+            Edit <code>src/VistaUsuario.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+  
+  export default VistaUsuario;
